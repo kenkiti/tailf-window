ee6ce78f9c82f40466053b03215a3c2155ca08e5
kenkiti <bzn03142@hotmail.com>
2019-06-26 11:41:55 +0900

    プロジェクト ファイルを追加します。

---
+++ tailf/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace tailf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("ＭＳ ゴシック", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(518, 164);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tailToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(518, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tailToolStripMenuItem
+            // 
+            this.tailToolStripMenuItem.Name = "tailToolStripMenuItem";
+            this.tailToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.tailToolStripMenuItem.Text = "tail";
+            this.tailToolStripMenuItem.Click += new System.EventHandler(this.TailToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 19);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "FilePath";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 188);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "tailf-window";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tailToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ tailf/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace tailf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = @"\\MARKETSPEED005\log\" + DateTime.Now.ToString("yyyyMMdd") + "cs.txt";
+        }
+
+        private void Tail(string filename)
+        {
+            FileInfo fi = new FileInfo(filename);
+            using (FileStream stream = fi.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                Boolean state = false;
+                int size = (int)fi.Length;
+                stream.Seek(size, SeekOrigin.Current);
+                using (FileSystemWatcher fw = new FileSystemWatcher(fi.DirectoryName, fi.Name))
+                {
+                    Action<Object, FileSystemEventArgs> ReadText = (sender, e) =>
+                    {
+                        fi.Refresh();
+                        Byte[] al = new Byte[] { };
+                        int RemainingSize = (int)fi.Length - size;
+                        if (RemainingSize <= 0) return;
+                        Array.Resize<Byte>(ref al, RemainingSize);
+                        int result = stream.Read(al, 0, RemainingSize);
+                        size = (int)fi.Length;
+                        Invoke(new Action(() =>
+                        {
+                            richTextBox1.AppendText(Encoding.GetEncoding("sjis").GetString(al));
+                        }));
+                    };
+                    fw.InternalBufferSize = 4*4096;
+                    fw.NotifyFilter = NotifyFilters.Size | NotifyFilters.LastWrite | NotifyFilters.LastAccess;
+                    fw.Changed += new FileSystemEventHandler(ReadText);
+                    fw.EnableRaisingEvents = true;
+                    while (!state) ;
+                }
+            }
+        }
+
+        private void TailToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path = textBox1.Text;
+            richTextBox1.AppendText(path+Environment.NewLine);
+
+            if (File.Exists(path))
+            {
+                Task.Run(() => Tail(textBox1.Text));
+            } else
+            {
+                richTextBox1.AppendText("File not found.");
+            }
+
+        }
+    }
+}
